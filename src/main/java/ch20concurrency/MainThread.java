127a334647bf608c91cf82c8235d47b2f17377d9
ViaPro <zjuwmh@163.com>
2013-11-02 16:00:40 +0800

    intro

---
+++ src/main/java/ch20concurrency/MainThread.java
@@ -0,0 +1,11 @@
+package ch20concurrency;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ViaPro
+ * Date: 11/2/13
+ * Time: 1:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainThread {
+}
