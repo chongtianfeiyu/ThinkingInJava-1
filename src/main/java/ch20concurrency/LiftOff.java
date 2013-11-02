1c469d9fbc513468a124238405cd956f4d5b8283
ViaPro <zjuwmh@163.com>
2013-11-02 13:49:24 +0800

    first commit

---
+++ src/main/java/ch20concurrency/LiftOff.java
@@ -0,0 +1,27 @@
+package ch20concurrency;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ViaPro
+ * Date: 11/2/13
+ * Time: 1:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LiftOff {
+    protected int countDown = 10;
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+    public LiftOff(){}
+    public LiftOff(int countDown){
+        this.countDown=countDown;
+    }
+    public String status(){
+        return "#" +id +"(" +(countDown>0?countDown:"LiftOff!")+"),";
+    }
+    public void run(){
+        while (countDown-->0){
+            System.out.print(status());
+            Thread.yield();
+        }
+    }
+}
