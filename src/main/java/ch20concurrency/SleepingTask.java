8bc9cca8141e74cd7082fc960e73366753f6130a
ViaPro <zjuwmh@163.com>
2013-11-05 10:49:12 +0800

    TimeUnit

---
+++ src/main/java/ch20concurrency/SleepingTask.java
@@ -0,0 +1,33 @@
+package ch20concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ViaPro
+ * Date: 11/5/13
+ * Time: 10:38 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SleepingTask extends LiftOff {
+    @Override
+    public void run() {
+        try {
+            while (countDown-- > 0) {
+                System.out.print(status());
+                TimeUnit.MILLISECONDS.sleep(800);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++)
+            exec.execute(new SleepingTask());
+        exec.shutdown();
+    }
+}
