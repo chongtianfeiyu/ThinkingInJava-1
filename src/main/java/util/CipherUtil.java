f8eac46a13b612e6179d793e5bd6a12b4f7cb099
ViaPro <zjuwmh@gmail.com>
2013-12-02 09:53:38 +0800

    cipher

---
+++ src/main/java/util/CipherUtil.java
@@ -0,0 +1,29 @@
+package util;
+
+import concurrency.Immutable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ViaPro
+ * Date: 13-12-2
+ * Time: 上午9:30
+ */
+
+@Immutable
+public class CipherUtil {
+    public static byte[] getKc(){
+        return null;
+    }
+    private static byte[] mac(){
+        return null;
+    }
+    private static byte[] random(){
+        return null;
+    }
+    //补充不足位
+    private static byte[] comDate(){
+        return null;
+    }
+
+
+}
